1e00b894ed832d41fbbbb1ec1f92a699f5de7ee3
HanzalaMehmoodButt <hanzala.butt24@gmail.com>
2020-07-22 17:45:59 +0500

    latest post implemented in admin dashboard

--- src/app/dashboards/dashboard-components/recent-comments/comment.component.ts
+++ src/app/dashboards/dashboard-components/recent-comments/comment.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+import { BlogService } from "../../../blog/services/blog.service";
+import { environment } from "src/environments/environment";
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.css']
 })
-export class CommentComponent {
+export class CommentComponent implements OnInit {
   public config: PerfectScrollbarConfigInterface = {};
-
-  constructor() {}
+  imgUrl = environment.imgUrl;
+  posts;
+  constructor(private blogS:BlogService) { }
+  ngOnInit() {
+    this.blogS.getAllPosts({}).subscribe(res => {
+      console.log(res)
+      this.posts = res.data
+    })
+  }
 }
