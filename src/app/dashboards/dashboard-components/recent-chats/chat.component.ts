4b64e8ed4fe40170675f1d6a5e28925287fd5bcb
HanzalaMehmoodButt <hanzala.butt24@gmail.com>
2020-07-22 18:09:36 +0500

    latest recipes implemented in admin dashboard

--- src/app/dashboards/dashboard-components/recent-chats/chat.component.ts
+++ src/app/dashboards/dashboard-components/recent-chats/chat.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, AfterViewInit } from '@angular/core';
 
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
-
+import { environment } from "src/environments/environment";
+import { RecipeService } from "../../../recipe/services/recipe.service"
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
-  public config: PerfectScrollbarConfigInterface = {};
+export class ChatComponent implements OnInit{
+ 
+  recipes;
+  imgUrl = environment.imgUrl;
 
-  constructor() {}
+  public config: PerfectScrollbarConfigInterface = {};
+  constructor(private recipeS:RecipeService) { }
+  
+  ngOnInit() {
+    this.recipeS.getAllRecipes({}).subscribe(res => {
+      this.recipes = res.data;
+      console.log(this.recipes)
+    })
+  }
 }
