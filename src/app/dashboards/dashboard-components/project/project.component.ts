3d257a2f422d6cf4cadf933d3d0edf7a07e4c1e0
HanzalaMehmoodButt <hanzala.butt24@gmail.com>
2020-07-22 17:03:06 +0500

    partner's added in dashboard

--- src/app/dashboards/dashboard-components/project/project.component.ts
+++ src/app/dashboards/dashboard-components/project/project.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from "../../services/dashboard.service";
+import { environment } from "src/environments/environment";
+
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html'
 })
-export class ProjectComponent {
-  constructor() {}
+export class ProjectComponent implements OnInit {
+  partners;
+  imgUrl = environment.imgUrl;
+  constructor(private dashS: DashboardService) { }
+  ngOnInit() {
+    this.dashS.getAllPartners().subscribe(res => {
+      console.log(res)
+      this.partners = res.users
+    });
+  }
 }
