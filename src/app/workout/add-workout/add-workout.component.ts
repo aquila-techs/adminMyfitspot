f4ec608a4ebcd60038730591b24a3288f0cf3423
Rushabh Shah <rushabh.shah@ordextechnology.com>
2021-04-24 12:31:55 +0530

    publish and modify workout issue fixes.

--- src/app/workout/add-workout/add-workout.component.ts
+++ src/app/workout/add-workout/add-workout.component.ts
@@ -3,137 +3,223 @@ import Quill from 'quill';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { ToastrService } from 'ngx-toastr';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { WorkoutService } from '../services/workout.service';
-import { NgbModal, ModalDismissReasons, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
-import { environment } from "src/environments/environment";
+import {
+  NgbModal,
+  ModalDismissReasons,
+  NgbActiveModal,
+} from '@ng-bootstrap/ng-bootstrap';
+import { environment } from 'src/environments/environment';
 import { Subscription } from 'rxjs';
 import { SharedService } from '../services/shared.service';
 
 @Component({
   selector: 'app-add-workout',
   templateUrl: './add-workout.component.html',
-  styleUrls: ['./add-workout.component.scss']
+  styleUrls: ['./add-workout.component.scss'],
 })
 export class AddWorkoutComponent implements OnInit {
-
   workout = {
-    nameEn: "", nameNl: "", workoutUrl: "", time: "", videoUrl: "", type: "", specification: '',
-    difficulityLevel: "", sweatFactor: "", categories: [], equipment: [], descriptionEn: "",
-    descriptionNl: "", muscleGroup: "", excercises: [] , pricing: "", timeLines: []
-  }
+    nameEn: '',
+    nameNl: '',
+    workoutUrl: '',
+    time: '',
+    videoUrl: '',
+    type: '',
+    specification: '',
+    difficulityLevel: '',
+    sweatFactor: '',
+    categories: [],
+    equipment: [],
+    descriptionEn: '',
+    descriptionNl: '',
+    muscleGroup: '',
+    excercises: [],
+    pricing: '',
+    timeLines: [],
+  };
   env = environment.imgUrl;
 
-  diffculity = ["None", "Gluteband", "Resistance Band", "Dumbell","Barbell", "Rings", "Cable station", "Acces to gym"];
-  sweat = ["Beginner", "Intermediate", "Advanced","Pro"];
-  specification = ["Full body", "Core", "Glutes", "Upper body", "Lower body"];
-  categories = ["Fit body", "Fat Burning"]
+  equipment = [
+    'None',
+    'Gluteband',
+    'Resistance Band',
+    'Dumbell',
+    'Barbell',
+    'Rings',
+    'Cable station',
+    'Acces to gym',
+  ];
+  diffculity = ['Beginner', 'Intermediate', 'Advanced', 'Pro'];
+  specification = ['Full body', 'Core', 'Glutes', 'Upper body', 'Lower body'];
+  categories = ['Fit body', 'Fat Burning'];
   // muscle = ["Quadriceps","Glutes","Hamstring","Chest","Lats","Shoulder","Biceps","Triceps","Core","Calfs","Upper Traps","Bag","Mobility"]
   muscle = [] as any;
   fileName;
   file;
-  imageUrl: string | ArrayBuffer = "";
+  imageUrl: string | ArrayBuffer = '';
   musclesChild = [] as any;
-  
+
   subscription: Subscription;
 
   public config: PerfectScrollbarConfigInterface = {};
-  constructor(private toastr: ToastrService, private router: Router, private workoutS: WorkoutService, private modalService: NgbModal,
-    private sharedService: SharedService) {
-    
-  }
-
+  constructor(
+    private toastr: ToastrService,
+    private router: Router,
+    private workoutS: WorkoutService,
+    private modalService: NgbModal,
+    private sharedService: SharedService
+  ) { }
 
   ngOnInit(): void {
-    this.subscription = this.sharedService.getMessage().subscribe(message => {
-      if (message) {
-        this.workout = message;
+    this.subscription = this.sharedService.getMessage().subscribe(
+      (message) => {
+        console.log(message);
+        if (message) {
+          this.workout = message;
+        }
+      },
+      (error) => {
+        console.log('error', error);
       }
-    }) 
+    );
     this.getAllParentMuscles();
   }
 
-
   onChange(file: File) {
+    console.log(file);
     if (file) {
       this.fileName = file.name;
       this.file = file;
       const reader = new FileReader();
       reader.readAsDataURL(file);
-      reader.onload = event => {
+      reader.onload = (event) => {
         this.imageUrl = reader.result;
+        console.log('image url === ', this.imageUrl);
       };
     }
   }
 
   Addworkout() {
-    this.workoutS.createWorkout(this.workout, this.file).subscribe(res => {
+    console.log(this.workout);
+    this.workoutS.createWorkout(this.workout, this.file).subscribe((res) => {
       if (res.status == true) {
-        this.toastr.success("WorkOut Published!", 'Success!', { timeOut: 2000, closeButton: true, progressBar: true, progressAnimation: 'decreasing' });
-        setTimeout(() => this.router.navigateByUrl('/workout/all'), 500)
+        this.toastr.success('WorkOut Published!', 'Success!', {
+          timeOut: 2000,
+          closeButton: true,
+          progressBar: true,
+          progressAnimation: 'decreasing',
+        });
+        setTimeout(() => this.router.navigateByUrl('/workout/all'), 500);
       } else {
-        this.toastr.error(res.message, 'Oops!', { timeOut: 2000, closeButton: true, progressBar: true, progressAnimation: 'decreasing' });
+        this.toastr.error(res.message, 'Oops!', {
+          timeOut: 2000,
+          closeButton: true,
+          progressBar: true,
+          progressAnimation: 'decreasing',
+        });
       }
-    })
+    });
   }
 
   getAllParentMuscles() {
-    this.workoutS.getAllParentCategories().subscribe(res => {
-      this.muscle = res.data;
-      this.muscle['quadriceps'] = this.muscle.filter(x=>x.categoryType == 'quadriceps');
-      this.muscle['glutes'] = this.muscle.filter(x=>x.categoryType == 'glutes');
-      this.muscle['hamstring'] = this.muscle.filter(x=>x.categoryType == 'hamstring');
-      this.muscle['chest'] = this.muscle.filter(x=>x.categoryType == 'chest');
-      this.muscle['lats'] = this.muscle.filter(x=>x.categoryType == 'lats');
-      this.muscle['shoulder'] = this.muscle.filter(x=>x.categoryType == 'shoulder');
-      this.muscle['biceps'] = this.muscle.filter(x=>x.categoryType == 'biceps');
-      this.muscle['triceps'] = this.muscle.filter(x=>x.categoryType == 'triceps');
-      this.muscle['core'] = this.muscle.filter(x=>x.categoryType == 'core');
-      this.muscle['calfs'] = this.muscle.filter(x=>x.categoryType == 'calfs');
-      this.muscle['upperTraps'] = this.muscle.filter(x=>x.categoryType == 'upperTraps');
-      this.muscle['bag'] = this.muscle.filter(x=>x.categoryType == 'bag');
-      this.muscle['mobility'] = this.muscle.filter(x=>x.categoryType == 'mobility');
-    })
+    this.workoutS.getAllParentCategories().subscribe(
+      (res) => {
+        this.muscle = res.data;
+        this.muscle['quadriceps'] = this.muscle.filter(
+          (x) => x.categoryType == 'quadriceps'
+        );
+        this.muscle['glutes'] = this.muscle.filter(
+          (x) => x.categoryType == 'glutes'
+        );
+        this.muscle['hamstring'] = this.muscle.filter(
+          (x) => x.categoryType == 'hamstring'
+        );
+        this.muscle['chest'] = this.muscle.filter(
+          (x) => x.categoryType == 'chest'
+        );
+        this.muscle['lats'] = this.muscle.filter((x) => x.categoryType == 'lats');
+        this.muscle['shoulder'] = this.muscle.filter(
+          (x) => x.categoryType == 'shoulder'
+        );
+        this.muscle['biceps'] = this.muscle.filter(
+          (x) => x.categoryType == 'biceps'
+        );
+        this.muscle['triceps'] = this.muscle.filter(
+          (x) => x.categoryType == 'triceps'
+        );
+        this.muscle['core'] = this.muscle.filter((x) => x.categoryType == 'core');
+        this.muscle['calfs'] = this.muscle.filter(
+          (x) => x.categoryType == 'calfs'
+        );
+        this.muscle['upperTraps'] = this.muscle.filter(
+          (x) => x.categoryType == 'upperTraps'
+        );
+        this.muscle['bag'] = this.muscle.filter((x) => x.categoryType == 'bag');
+        this.muscle['mobility'] = this.muscle.filter(
+          (x) => x.categoryType == 'mobility'
+        );
+      });
   }
 
   openchildMuscle(openChildMuscles, id, name) {
     this.workout.muscleGroup = '';
     this.workout.excercises = [];
     this.workout.muscleGroup = name;
-    this.workoutS.getAllParentChildCategories(id).subscribe(res => {
+    this.workoutS.getAllParentChildCategories(id).subscribe((res) => {
       this.musclesChild = res.data;
-      this.modalService.open(openChildMuscles, { windowClass: "dark-modal", size: 'lg',centered: true })
-    })
+      this.modalService.open(openChildMuscles, {
+        windowClass: 'dark-modal',
+        size: 'lg',
+        centered: true,
+      });
+    });
   }
 
   addExcersise(id) {
-      this.workout.excercises.push(id); 
+    this.workout.excercises.push(id);
   }
   removeExcersise(id) {
-    this.workout.excercises = this.workout.excercises.filter(ele => ele != id);
+    this.workout.excercises = this.workout.excercises.filter(
+      (ele) => ele != id
+    );
   }
-  
+
   addCategory(c) {
     let check = this.workout.categories.includes(c);
     if (check === true) {
-      this.workout.categories = this.workout.categories.filter(ele => ele != c);
+      this.workout.categories = this.workout.categories.filter(
+        (ele) => ele != c
+      );
     } else {
       this.workout.categories.push(c);
     }
   }
 
-  deleteTimeline(index: any){
-      this.workout.timeLines.splice(index, 1);
+  addEquipment(e) {
+    console.log(this.workout.equipment.length)
+    // add unique equipment in array logic
+    if (this.workout.equipment.length > 0) {
+      this.workout.equipment.pop();
+      this.workout.equipment.push(e);
+    } else {
+      this.workout.equipment.push(e);
+    }
+    console.log(this.workout.equipment)
   }
 
-  addToTimeline(item: any){
+  deleteTimeline(index: any) {
+    this.workout.timeLines.splice(index, 1);
+  }
+
+  addToTimeline(item: any) {
     this.workout.timeLines.push(item.name);
-}
+  }
 
   ngOnDestroy() {
     // unsubscribe to ensure no memory leaks
     this.subscription.unsubscribe();
   }
-
 }
--- src/app/workout/modify-workout/modify-workout.component.ts
+++ src/app/workout/modify-workout/modify-workout.component.ts
@@ -1,12 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import Quill from 'quill';
 import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
-import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from "@angular/router";
-import { NgForm } from '@angular/forms';
 import { WorkoutService } from '../services/workout.service';
-import { NgbModal, ModalDismissReasons, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { environment } from "src/environments/environment";
 import { Subscription } from 'rxjs';
 import { SharedService } from '../services/shared.service';
@@ -21,12 +18,11 @@ export class ModifyWorkoutComponent implements OnInit {
   workout = {
     nameEn: "", nameNl: "", workoutUrl: "", time: "", videoUrl: "", type: "", specification: '',
     difficulityLevel: "", sweatFactor: "", categories: [], equipment: [], descriptionEn: "",
-    descriptionNl: "", muscleGroup: "", excercises: [] , pricing: "", timeLines: []
+    descriptionNl: "", muscleGroup: "", excercises: [], pricing: "", timeLines: []
   }
   env = environment.imgUrl;
-
-  diffculity = ["None", "Gluteband", "Resistance Band", "Dumbell","Barbell", "Rings", "Cable station", "Acces to gym"];
-  sweat = ["Beginner", "Intermediate", "Advanced","Pro"];
+  equipment = ["None", "Gluteband", "Resistance Band", "Dumbell", "Barbell", "Rings", "Cable station", "Acces to gym"];
+  difficulty = ["Beginner", "Intermediate", "Advanced", "Pro"];
   specification = ["Full body", "Core", "Glutes", "Upper body", "Lower body"];
   categories = ["Fit body", "Fat Burning"]
   // muscle = ["Quadriceps","Glutes","Hamstring","Chest","Lats","Shoulder","Biceps","Triceps","Core","Calfs","Upper Traps","Bag","Mobility"]
@@ -35,20 +31,24 @@ export class ModifyWorkoutComponent implements OnInit {
   file;
   imageUrl: string | ArrayBuffer = "";
   musclesChild = [] as any;
-  
   subscription: Subscription;
-
   rememberMe: string;
+  fitBody = false;
+  fatBurning = false;
 
   public config: PerfectScrollbarConfigInterface = {};
+
   constructor(private toastr: ToastrService, private router: Router, private workoutS: WorkoutService, private modalService: NgbModal,
     private sharedService: SharedService) {
-      this.subscription = this.sharedService.getMessage().subscribe(message => {
-        if (message) {
-          this.workout = message;
-          this.workout['_id'] = message._id;
-        }
-      }) 
+    this.subscription = this.sharedService.getMessage().subscribe(message => {
+      if (message) {
+        this.workout = message;
+        console.log(this.workout);
+        this.workout.categories.includes('Fit body') ? this.fitBody = true : this.fitBody = false;
+        this.workout.categories.includes('Fat Burning') ? this.fatBurning = true : this.fatBurning = false;
+        this.workout['_id'] = message._id;
+      }
+    })
   }
 
 
@@ -70,6 +70,7 @@ export class ModifyWorkoutComponent implements OnInit {
   }
 
   modifyWorkout() {
+    console.log(this.workout);
     this.workoutS.modifyWorkout(this.workout, this.file).subscribe(res => {
       if (res.status == true) {
         this.toastr.success("WorkOut Modified!", 'Success!', { timeOut: 2000, closeButton: true, progressBar: true, progressAnimation: 'decreasing' });
@@ -83,19 +84,19 @@ export class ModifyWorkoutComponent implements OnInit {
   getAllParentMuscles() {
     this.workoutS.getAllParentCategories().subscribe(res => {
       this.muscle = res.data;
-      this.muscle['quadriceps'] = this.muscle.filter(x=>x.categoryType == 'quadriceps');
-      this.muscle['glutes'] = this.muscle.filter(x=>x.categoryType == 'glutes');
-      this.muscle['hamstring'] = this.muscle.filter(x=>x.categoryType == 'hamstring');
-      this.muscle['chest'] = this.muscle.filter(x=>x.categoryType == 'chest');
-      this.muscle['lats'] = this.muscle.filter(x=>x.categoryType == 'lats');
-      this.muscle['shoulder'] = this.muscle.filter(x=>x.categoryType == 'shoulder');
-      this.muscle['biceps'] = this.muscle.filter(x=>x.categoryType == 'biceps');
-      this.muscle['triceps'] = this.muscle.filter(x=>x.categoryType == 'triceps');
-      this.muscle['core'] = this.muscle.filter(x=>x.categoryType == 'core');
-      this.muscle['calfs'] = this.muscle.filter(x=>x.categoryType == 'calfs');
-      this.muscle['upperTraps'] = this.muscle.filter(x=>x.categoryType == 'upperTraps');
-      this.muscle['bag'] = this.muscle.filter(x=>x.categoryType == 'bag');
-      this.muscle['mobility'] = this.muscle.filter(x=>x.categoryType == 'mobility');
+      this.muscle['quadriceps'] = this.muscle.filter(x => x.categoryType == 'quadriceps');
+      this.muscle['glutes'] = this.muscle.filter(x => x.categoryType == 'glutes');
+      this.muscle['hamstring'] = this.muscle.filter(x => x.categoryType == 'hamstring');
+      this.muscle['chest'] = this.muscle.filter(x => x.categoryType == 'chest');
+      this.muscle['lats'] = this.muscle.filter(x => x.categoryType == 'lats');
+      this.muscle['shoulder'] = this.muscle.filter(x => x.categoryType == 'shoulder');
+      this.muscle['biceps'] = this.muscle.filter(x => x.categoryType == 'biceps');
+      this.muscle['triceps'] = this.muscle.filter(x => x.categoryType == 'triceps');
+      this.muscle['core'] = this.muscle.filter(x => x.categoryType == 'core');
+      this.muscle['calfs'] = this.muscle.filter(x => x.categoryType == 'calfs');
+      this.muscle['upperTraps'] = this.muscle.filter(x => x.categoryType == 'upperTraps');
+      this.muscle['bag'] = this.muscle.filter(x => x.categoryType == 'bag');
+      this.muscle['mobility'] = this.muscle.filter(x => x.categoryType == 'mobility');
     })
   }
 
@@ -105,31 +106,35 @@ export class ModifyWorkoutComponent implements OnInit {
     this.workout.muscleGroup = name;
     this.workoutS.getAllParentChildCategories(id).subscribe(res => {
       this.musclesChild = res.data;
-      this.modalService.open(openChildMuscles, { windowClass: "dark-modal", size: 'lg',centered: true })
+      this.modalService.open(openChildMuscles, { windowClass: "dark-modal", size: 'lg', centered: true })
     })
   }
 
   addExcersise(id) {
-      this.workout.excercises.push(id); 
+    this.workout.excercises.push(id);
   }
   removeExcersise(id) {
     this.workout.excercises = this.workout.excercises.filter(ele => ele != id);
   }
-  
+
   addCategory(c) {
-    let check = this.workout.categories.includes(c);
-    if (check === true) {
-      this.workout.categories = this.workout.categories.filter(ele => ele != c);
+    console.log(c)
+    if (this.workout.categories.includes(c)) {
+      let i = this.workout.categories.indexOf(c)
+      if (i > -1) {
+        this.workout.categories.splice(i, 1);
+      }
     } else {
       this.workout.categories.push(c);
     }
+    console.log(this.workout.categories)
   }
 
-  deleteTimeline(index: any){
-    this.workout.timeLines.splice(index, 1);    
-}
+  deleteTimeline(index: any) {
+    this.workout.timeLines.splice(index, 1);
+  }
 
-  addToTimeline(item: any){
+  addToTimeline(item: any) {
     this.workout.timeLines.push(item.name);
   }
 
